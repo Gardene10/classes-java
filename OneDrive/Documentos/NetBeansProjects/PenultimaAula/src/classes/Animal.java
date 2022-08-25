a48387039b129a8b9b2bdcc4d886ccaba99eaf96
Gardene <>
2022-08-25 15:18:24 -0300

    tudo ok

---
+++ OneDrive/Documentos/NetBeansProjects/PenultimaAula/src/classes/Animal.java
@@ -0,0 +1,40 @@
+
+package classes;
+
+public abstract class Animal {
+    //Atributos de animail
+    protected float peso;
+    protected int idade;
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getMembros() {
+        return membros;
+    }
+
+    public void setMembros(int membros) {
+        this.membros = membros;
+    }
+    protected int membros;
+    
+    // metodos de animal
+    public abstract void locomover();
+    public abstract void alimentar();
+    public abstract void emitirSom();
+    
+    
+}
---
+++ OneDrive/Documentos/NetBeansProjects/PenultimaAula/src/classes/Arara.java
@@ -0,0 +1,6 @@
+
+package classes;
+
+public class Arara extends Ave {
+    
+}
